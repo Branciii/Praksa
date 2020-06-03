3494842abd90ccd8c0e28f27d2d4b536abcffe13
Branciii <branka.brkic.799@gmail.com>
2020-06-03 12:24:37 +0200

    ssms tablice, insert u tablice i screenovi

--- Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace PraksaWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
---
+++ PraksaSQL/PraksaSQLInsert.sql
@@ -0,0 +1,34 @@
+
+INSERT INTO KUCANSTVO VALUES (5, 'Vukovarska', 100, 'Osijek', 'Hrvatska');
+INSERT INTO KUCANSTVO (id, ulica, kucni_broj, grad) VALUES (6, 'Vukovarska', 150, 'Osijek');
+INSERT INTO KUCANSTVO (id, ulica, kucni_broj, grad) VALUES (7, 'Cvjetna', 10, 'Osijek');
+INSERT INTO KUCANSTVO VALUES (8, 'Rikarda Benèiæa', 11, 'Rijeka', 'Hrvatska');
+
+
+INSERT INTO FAKULTET VALUES (9, 'Odjel za matematiku', 'Trg Ljudevita Gaja 6','Osijek', 'Hrvatska');
+INSERT INTO FAKULTET VALUES (10, 'Odjel za biologiju', 'cara Hadrijana 8/A','Osijek', 'Hrvatska');
+INSERT INTO FAKULTET VALUES (11, 'Odjel za kemiju', 'cara Hadrijana 8/A','Osijek', 'Hrvatska');
+INSERT INTO FAKULTET VALUES (12, 'Odjel za fiziku', 'Trg Ljudevita Gaja 6','Osijek', 'Hrvatska');
+
+INSERT INTO STUDENT VALUES (1, 'Pero', 'Peric', '09/07/1999', 5, 9);
+INSERT INTO STUDENT VALUES (2, 'Ana', 'Anic', '09/07/1999', 6, 10);
+INSERT INTO STUDENT VALUES (3, 'Luka', 'Lukic', '09/07/1999', 6, 11);
+INSERT INTO STUDENT VALUES (4, 'Eva', 'Evic', '09/07/1999', 7, 12);
+
+INSERT INTO INDEKS VALUES (1,8000);
+INSERT INTO INDEKS VALUES (2, 8500);
+INSERT INTO INDEKS VALUES (3, 7999);
+INSERT INTO INDEKS VALUES (4, 9000);
+
+INSERT INTO KOLEGIJ VALUES (13, 'Osnove teorije upravljanja', 7, 9);
+INSERT INTO KOLEGIJ VALUES (14, 'Realna analiza', 7, 9);
+INSERT INTO KOLEGIJ VALUES (15, 'Strojno uèenje', 7, 9);
+INSERT INTO KOLEGIJ VALUES (16, 'Numerièka matematika', 6, 9);
+
+INSERT INTO KOLEGIJ_STUDENT VALUES (13,1);
+INSERT INTO KOLEGIJ_STUDENT VALUES (13,4);
+INSERT INTO KOLEGIJ_STUDENT VALUES (13,3);
+INSERT INTO KOLEGIJ_STUDENT VALUES (13,2);
+INSERT INTO KOLEGIJ_STUDENT VALUES (14,2);
+INSERT INTO KOLEGIJ_STUDENT VALUES (15,3);
+INSERT INTO KOLEGIJ_STUDENT VALUES (16,4);
